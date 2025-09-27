6e1c3a1e7384c586029646a80d3855f3bda72fd7
Aniket Kolate <aniket.kolate@cision.com>
2025-09-27 21:45:20 +0530

    feat: add About, Contact, Projects, and Skills components with respective styles

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Contact.scss";
+
+const Contact: React.FC = () => {
+    return (
+        <div className="contact-container">
+            <h1>Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
