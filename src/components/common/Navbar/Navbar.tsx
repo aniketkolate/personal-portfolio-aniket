1b83ca2ee3b0a8caacb6be6b51fb6e2c7acc8676
aniketkolate <aniketkolte18@gmail.com>
2025-10-01 01:12:29 +0530

    fix: Update home page style

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,53 +1,59 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
+import menuIcon from '@/assets/icons/menu.png';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar-container">
-      <ul className="navbar-links">
-        <li>
-          <NavLink
-            to="/home"
-            className={({ isActive }) => isActive ? "active" : ""}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => isActive ? "active" : ""}
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/skills"
-            className={({ isActive }) => isActive ? "active" : ""}
-          >
-            Skills
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/projects"
-            className={({ isActive }) => isActive ? "active" : ""}
-          >
-            Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => isActive ? "active" : ""}
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar-container">
+        <ul className="navbar-links">
+          <li>
+            <NavLink
+              to="/home"
+              className={({ isActive }) => isActive ? "active" : ""}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => isActive ? "active" : ""}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/skills"
+              className={({ isActive }) => isActive ? "active" : ""}
+            >
+              Skills
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) => isActive ? "active" : ""}
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) => isActive ? "active" : ""}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <nav className="navbar-container-mobile">
+        <img src={menuIcon} alt="Menu" />
+      </nav>
+    </>
   );
 };
 
